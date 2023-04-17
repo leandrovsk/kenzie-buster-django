01b56aad1d6f055f65f6f28075fdc09bca233abe
leandrovsk <contas.center@gmail.com>
2023-04-17 02:24:46 -0300

    feat: task 1 completed

---
+++ users/views.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView, Request, Response, status
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserView(APIView):
+    def get(self, request: Request) -> Response:
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, request: Request) -> Response:
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
